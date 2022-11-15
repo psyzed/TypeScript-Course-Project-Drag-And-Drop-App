e1ede7714c6e9b407ae61dcf711b6f178cee3d6a
Lazaros Poumpouridis <psyzed66@gmail.com>
2022-11-15 13:06:11 +0200

    extracted user input

--- src/app.ts
+++ src/app.ts
@@ -50,10 +50,39 @@ class ProjectInput {
     this.attach();
   }
 
+  private gatherUserInput(): [string, string, number] | void {
+    const enteredTitle = this.titleInputElement.value;
+    const enteredDescription = this.descriptionInputElement.value;
+    const enteredPeople = this.peopleInputElement.value;
+
+    if (
+      enteredTitle.trim().length === 0 ||
+      enteredDescription.trim().length === 0 ||
+      enteredPeople.trim().length === 0
+    ) {
+      alert("Invalid Input!");
+      return;
+    } else {
+      return [enteredTitle, enteredDescription, +enteredPeople];
+    }
+  }
+
+  private clearInputs() {
+    this.titleInputElement.value = "";
+    this.descriptionInputElement.value = "";
+    this.peopleInputElement.value = "";
+  }
+
   @BindThis
   private submitHandler(event: Event) {
     event.preventDefault(); //preventing http request.
-    console.log(this.titleInputElement.value);
+    const userInput = this.gatherUserInput();
+    if (Array.isArray(userInput)) {
+      const [title, desc, people] = userInput;
+      console.log(title, desc, people);
+      console.log(userInput);
+    }
+    this.clearInputs();
   }
 
   private configure() {
